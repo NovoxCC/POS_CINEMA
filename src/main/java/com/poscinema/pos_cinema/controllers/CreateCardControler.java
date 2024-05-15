fcfca52094f094f104f9031a6a0fadbf3c5fea77
Camilo Moreno <camlo_1250@hotmail.com>
2024-05-15 12:01:27 -0500

    user modification
    The user was modified so that he can store a total in cash

--- src/main/java/com/poscinema/pos_cinema/controllers/CreateCardControler.java
+++ src/main/java/com/poscinema/pos_cinema/controllers/CreateCardControler.java
@@ -31,24 +31,29 @@ public class CreateCardControler {
     System.out.println(cleanAndParseInt(totalpayField));
         // Verificar si el TextField no está vacío
         if (!idField.getText().isEmpty()) {
-            // El TextField no está vacío
             // Verificar si el TextField contiene solo números
             if (idField.getText().matches("\\d+")) {
-                // El TextField contiene solo números
-                try {
-                    // Cargar el nuevo formulario
-                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/poscinema/pos_cinema/payment-card.fxml"));
-                    Parent centerContent = loader.load();
+                //verificar si el TextField contiene un id con la longitud valida
+                if (idField.getText().length() > 6 ){
+                    // El TextField contiene solo números
+                    try {
+                        // Cargar el nuevo formulario
+                        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/poscinema/pos_cinema/payment-card.fxml"));
+                        Parent centerContent = loader.load();
 
-                    // Obtener el BorderPane padre desde el botón que activó el evento
-                    Button button = (Button) actionEvent.getSource();
-                    BorderPane parentBorderPane = (BorderPane) button.getScene().getRoot();
+                        // Obtener el BorderPane padre desde el botón que activó el evento
+                        Button button = (Button) actionEvent.getSource();
+                        BorderPane parentBorderPane = (BorderPane) button.getScene().getRoot();
 
-                    // Establecer el nuevo contenido en el centro del BorderPane padre
-                    parentBorderPane.setCenter(centerContent);
-                } catch (IOException e) {
-                    showErrorDialog("Error loading view", "The view could not be loaded");
-                    // Manejo de errores
+                        // Establecer el nuevo contenido en el centro del BorderPane padre
+                        parentBorderPane.setCenter(centerContent);
+                    } catch (IOException e) {
+                        // Error al cargar la vista
+                        showErrorDialog("Error loading view", "The view could not be loaded");
+                    }
+                }else{
+                    // El TextField no contiene la catidad minima de numeros (6)
+                    showErrorDialog("Invalid id","The id isn't valid \n Is too short");
                 }
             } else {
                 // El TextField contiene caracteres que no son números
@@ -76,7 +81,8 @@ public class CreateCardControler {
             alert.setHeaderText(null);
             alert.setContentText(message);
 
-            // Obtener el stage padre y establecerlo como propietario de la alerta
+            // Obtener el stage padre y establecerlo como propietario de la alerta esto permite
+            // dar el mensaje sin que se pierda el foco y cambie de ventana
             Stage parentStage = getParentStage();
             alert.initOwner(parentStage);
             alert.initModality(Modality.WINDOW_MODAL);
--- src/main/java/com/poscinema/pos_cinema/models/User.java
+++ src/main/java/com/poscinema/pos_cinema/models/User.java
@@ -22,10 +22,12 @@ import java.util.logging.Logger;
 public class User {
     private String username;
     private Integer roleId;
+    private int balance;
 
     public User() {
         this.username = null;
         this.roleId = null;
+        this.balance = 0;
     }
 
     public String getUsername() {
@@ -42,6 +44,14 @@ public class User {
 
     public void setRoleId(int roleId) {
         this.roleId = roleId;
+    }
+
+    public  int getBalance(){
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
     }
 
     public static boolean login(String username, String password) {
